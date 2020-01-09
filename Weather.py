5e9336ebf20544fe477e3aca463f9139a7747b98
Viviancao <vivian.w.cao@gmail.com>
2020-01-09 10:56:59 -0800

    lab1

--- Weather.py
+++ Weather.py
@@ -1,4 +1,4 @@
-Temperature_Celcius=10
-Temperature_Fahrenheit=(Temperature_Celcius * 9/5) + 32
-print(Temperature_Celcius + "degrees")
-print(Temperature_Fahrenheit + "degrees")
+celcius = 10
+fahrenheit = 9/5 * celcius + 32
+print("Temperature:" + str (celcius) + "degrees")
+print("Temperature:" + str (fahrenheit) + "degrees")
